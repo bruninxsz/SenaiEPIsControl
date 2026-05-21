803b99a02d6112304ffa8e31a94e9a4c4d124950
bruninxsz <brunohsp0306@gmail.com>
2026-05-21 19:15:01 -0300

    responsividadeAplicadaMenu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
 
-     <aside class="flex flex-col bg-gray-500 px-4 text-white w-[200px] h-screen">
+     <aside class="flex flex-col bg-gray-500 px-4 text-white w-[200px] min-h-screen sticky top-0">
 
         <router-link to="/Dashboard/Inventory" class="mt-8 hover:font-bold">Estoque</router-link>
 
